689207d121b83f9c566abc4403feefb658f0da27
Алексей <tov.kessler@yandex.ru>
2021-02-26 23:13:29 +0800

    Build

---
+++ build/precache-manifest.c4f99fe7d54e0edcb704756d7328c440.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "637cc85c2624bda94aa5",
+    "url": "./static/css/main.bb3f43a9.chunk.css"
+  },
+  {
+    "revision": "637cc85c2624bda94aa5",
+    "url": "./static/js/main.637cc85c.chunk.js"
+  },
+  {
+    "revision": "1ca6a9e4ee978b3b6061",
+    "url": "./static/css/1.70a55b3c.chunk.css"
+  },
+  {
+    "revision": "1ca6a9e4ee978b3b6061",
+    "url": "./static/js/1.1ca6a9e4.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a220424304642762314d0160a2789383",
+    "url": "./static/media/error.a2204243.png"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "./static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "6ec15b2e953144af805b7860e7ef3da5",
+    "url": "./index.html"
+  }
+];
