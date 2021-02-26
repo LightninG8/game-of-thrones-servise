7c1f7a1a03bb44f8d8361e9b3843834885b4af52
Алексей <tov.kessler@yandex.ru>
2021-02-27 00:23:00 +0800

    build

---
+++ build/precache-manifest.f878d41da69f23d4b6bab09311be254c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3a4dad6d7572bf4d9c3",
+    "url": "./static/css/main.bb3f43a9.chunk.css"
+  },
+  {
+    "revision": "b3a4dad6d7572bf4d9c3",
+    "url": "./static/js/main.b3a4dad6.chunk.js"
+  },
+  {
+    "revision": "1ca6a9e4ee978b3b6061",
+    "url": "./static/css/1.70a55b3c.chunk.css"
+  },
+  {
+    "revision": "1ca6a9e4ee978b3b6061",
+    "url": "./static/js/1.1ca6a9e4.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a220424304642762314d0160a2789383",
+    "url": "./static/media/error.a2204243.png"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "./static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "b76d848d47118d27fce397470e060b6c",
+    "url": "./index.html"
+  }
+];
